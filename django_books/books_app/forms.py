5a75f9c26b291f46c19aeccb72f382138124f931
Anastasiia <n.a.s.t.y.a28v@gmail.com>
2016-10-17 17:33:50 +0300

    RabbirMQ experiments.

--- django_books/books_app/forms.py
+++ django_books/books_app/forms.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from django import forms
 from .tasks import start_email
 from django.utils.translation import ugettext_lazy as _
@@ -19,10 +20,32 @@ class SearchForm(forms.Form):
 
     def start(self, lang):
         if self.data['time']:
-            if start_email.apply_async(
-                    args=[self.data, lang],
-                    soft_time_limit=float(self.data['time'])):
+            # result = start_email.apply_async(
+            #     args=[self.data, lang],
+            #     soft_time_limit=float(self.data['time']))
+            result = start_email.s(self.data, lang).apply_async(
+                soft_time_limit=float(self.data['time']))
+            # print(result.get(propagate=False))
+            # print(result.ready())
+            # self.bla(result)
+            if result:
                 return self.data
         else:
-            if start_email.apply_async(args=[self.data, lang]):
+            result = start_email.s(self.data, lang)
+            if result():
                 return self.data
+
+    @staticmethod
+    def bla(result):
+        while True:
+            if result:
+                if result.backend.get_result(result.id):
+                    print('Result: {}'.format(
+                        result.backend.get_status(result.id)))
+                    break
+                else:
+                    print('No results: {}'.format(
+                        result.backend.get_status(result.id)))
+                    time.sleep(0.5)
+            else:
+                break
--- django_books/django_books/settings.py
+++ django_books/django_books/settings.py
@@ -162,7 +162,9 @@ LOGGING = {
 }
 
 # CELERY SETTINGS
-BROKER_URL = 'redis://localhost:6379/0'
+# BROKER_URL = 'redis://localhost:6379/0'
+BROKER_URL = 'amqp://'
+CELERY_RESULT_BACKEND = 'rpc://'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
